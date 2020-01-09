e40f288e262236f46302e645c31dbecc74a7a36e
prathyusha <prathyusha@Akhil>
2020-01-09 18:56:41 -0500

    Added test case

---
+++ TestProject/src/java_Hackerrank/primitiveDataTypes.java
@@ -0,0 +1,36 @@
+package java_Hackerrank;
+import java.util.*;
+public class primitiveDataTypes {
+
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+		 System.out.println("Enter no of testing times ");
+	        int t=sc.nextInt();
+
+	        for(int i=0;i<t;i++)
+	        {
+
+	            try
+	            {
+	                System.out.println("Enter a number  ");
+	            	long x=sc.nextLong();
+	                System.out.println(x+" can be fitted in:");
+	                if(x>=-128 && x<=127)
+	                System.out.println("* byte");
+	                 if(x>=-32768 && x<=32767)
+	                System.out.println("* short");
+	                if ( x >= Math.pow(-2,31) && x<= (Math.pow(2,31)-1))
+	                System.out.println("* int");
+	                 if ( x >= Math.pow(-2,63) && x<= (Math.pow(2,63)-1))
+	                System.out.println("* long");
+	            }
+	            catch(Exception e)
+	            {
+	                System.out.println(sc.next()+" can't be fitted anywhere.");
+	            }
+
+
+	}
+
+}
+}
